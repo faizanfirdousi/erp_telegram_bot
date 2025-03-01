87d580e4e73e843d10e228e59cee28e8f3bf9cee
Faizan <faizanfirdousi.official@gmail.com>
2025-03-01 17:43:25 +0530

    deleted config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Telegram Bot Token from BotFather
-TELEGRAM_TOKEN = "876123272:AAFmca-po2bmQ1Z0d6BIfDsAptddQIREw18"
-
-# 2captcha API key
-CAPTCHA_API_KEY = "dfe25dfb57892e1f51ba087c3a92fcba"
-
-# ERP URL
-ERP_URL = "https://isquareit.akronsystems.com/pLogin.aspx" 
